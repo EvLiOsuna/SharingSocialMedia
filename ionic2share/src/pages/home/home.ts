b74913a77f00ef211114c672a7fe1c91dfef387d
EvLiOsuna <31911287+EvLiOsuna@users.noreply.github.com>
2018-11-12 23:00:27 -0700

    Primer commit

---
+++ ionic2share/src/pages/home/home.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { SocialSharing } from '@ionic-native/social-sharing';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,private sharingVar: SocialSharing) {
+    
+  }
+ whatsappShare(){
+    this.sharingVar.shareViaWhatsApp("Message via WhatsApp", null /*Image*/,  "https://pointdeveloper.com/" /* url */)
+      .then(()=>{
+        alert("Success");
+      },
+      ()=>{
+         alert("failed")
+      })
+  }
+ 
+  twitterShare(){
+    this.sharingVar.shareViaTwitter("Message via Twitter",null /*Image*/,"https://pointdeveloper.com")
+    .then(()=>{
+        alert("Success");
+      },
+      ()=>{
+         alert("failed")
+      })
+  }
+ 
+  facebookShare(){
+    this.sharingVar.shareViaFacebook("Message via Twitter",null /*Image*/,"https://pointdeveloper.com")
+    .then(()=>{
+        alert("Success");
+      },
+      ()=>{
+         alert("failed")
+      })
+  }
+ 
+  otherShare(){
+    this.sharingVar.share("Genral Share Sheet",null/*Subject*/,null/*File*/,"https://pointdeveloper.com")
+    .then(()=>{
+        alert("Success");
+      },
+      ()=>{
+         alert("failed")
+      })
+ 
+  }
+}
